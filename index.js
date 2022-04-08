4bb25115f1be242ba281562ac8a7218c3a2ec58e
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-08 18:15:07 +0100

    socket io defined

--- index.js
+++ index.js
@@ -39,3 +39,65 @@ try {
 } catch (error) {
   throw new Error(`Can't start Express: ${error}`)
 }
+
+io.on('connection', function(socket) {
+  console.log(socket.id + ' has connected')
+  console.log(room)
+
+  socket.on('message', data => {
+      data = JSON.parse(data)
+      if (!room[data.room]) {
+          room[data.room] = [socket.id]
+      } else if (room[data.room].length < 2) {
+          if (room[data.room].filter(i => i === socket.id).length === 0) {
+              room[data.room].push(socket.id)
+          }
+      }
+      // if (room[data.room].length === 2) {
+      //     for (s of room[data.room]) {
+      //         if (s !== socket.id && io.sockets.sockets[s] && room[data.room].length <= 2) {
+      //             io.sockets.sockets[s].emit('message', data.data)
+      //         }
+      //     }
+      // }
+  })
+
+  socket.on('join', (roomId) => {
+      console.log('join' + roomId)
+      if (room[roomId] && room[roomId].length === 2) {
+          socket.emit('reject', { error: 'Room is full' })
+      }
+  })
+
+  socket.on('leave', (roomId) => {
+      console.log('leave' + roomId)
+      for (s in room) {
+          for (let i = 0; i < room[s].length; i++) {
+              if (room[s][i] === roomId) {
+                  room[s].splice(i, 1)
+                  if (room[s].length == 0) {
+                      delete room[s]
+                  }
+                  break
+              }
+          }
+      }
+      console.log(room)
+  })
+
+  socket.on('disconnect', () => {
+      console.log(socket.id + ' has been disconnected')
+      for (s in room) {
+          for (let i = 0; i < room[s].length; i++) {
+              if (room[s][i] === socket.id) {
+                  room[s].splice(i, 1)
+                  if (room[s].length == 0) {
+                      delete room[s]
+                  }
+                  break
+              }
+          }
+      }
+      console.log(room)
+  })
+});
